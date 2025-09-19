589b6f0e62a3d4b20024129aebef2cb02033a158
Yae Miko <aloksinghkumar756@gmail.com>
2025-09-19 21:33:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,36 +3,39 @@ from os import environ,getenv
 import logging
 from logging.handlers import RotatingFileHandler
 
+#------------------------------------------------------------------------------------------------------------------------------------------------
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8241756077:AAGdwmKlQ0cQpzYkr4edaQqH4A_ofZ2pN3A")
 APP_ID = int(os.environ.get("APP_ID", "27704224"))
 API_HASH = os.environ.get("API_HASH", "c2e33826d757fe113bc154fcfabc987d")
-
+#------------------------------------------------------------------------------------------------------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002583602391"))
 OWNER = os.environ.get("OWNER", "Mikoyae756")
 OWNER_ID = int(os.environ.get("OWNER_ID", "7970350353"))
-PORT = os.environ.get("PORT", "1001")
+#------------------------------------------------------------------------------------------------------------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Koi:aloksingh@cluster0.86wo9.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
+PORT = os.environ.get("PORT", "1001")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
+#------------------------------------------------------------------------------------------------------------------------------------------------
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
+#------------------------------------------------------------------------------------------------------------------------------------------------
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
-
+PREMIUM_PIC = os.environ.get("PREMIUM_PIC", "https://graph.org/file/a79edc947c63cbf1a3286-a45024690cd008d6c9.jpg")
+#------------------------------------------------------------------------------------------------------------------------------------------------
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "teralink.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "ab00c04b6a0bcb51de3ff7300c4a8491387222b4")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "60"))
 TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
-
-# Super Premium System Control
+#------------------------------------------------------------------------------------------------------------------------------------------------
 SUPER_PREMIUM_ENABLED = os.environ.get("SUPER_PREMIUM_ENABLED", "True") == "True"
 SUPER_PREMIUM_PROTECT_CONTENT = os.environ.get("SUPER_PREMIUM_PROTECT_CONTENT", "False") == "True"
-
+#------------------------------------------------------------------------------------------------------------------------------------------------
 HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @Nova_Flix\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n  ..."
 ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/cosmic_freak>Yato</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/otakuflix_network>ᴏᴛᴀᴋᴜғʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n◈ ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/anime_cruise_netflix>ᴀɴɪᴍᴇ ᴄʀᴜɪsᴇ</a>\n◈ sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/webseries_flix>ᴡᴇʙsᴇʀɪᴇs ғʟɪx</a>\n◈ ᴀᴅᴜʟᴛ ᴍᴀɴʜᴡᴀ : <a href=h ..."
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {mention}\n\n<blockquote> ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {mention}\n\n<b><blockquote>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b></blockquote>")
-PREMIUM_PIC = os.environ.get("PREMIUM_PIC", "https://graph.org/file/a79edc947c63cbf1a3286-a45024690cd008d6c9.jpg")
-
+#------------------------------------------------------------------------------------------------------------------------------------------------
 CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
 
 <b>›› /dlt_time :</b> sᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
@@ -59,13 +62,14 @@ CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockqu
 <b>›› /remove_super_premium :</b> ʀᴇᴍᴏᴠᴇ sᴜᴘᴇʀ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀ
 <b>›› /super_premium_users :</b> ʟɪsᴛ sᴜᴘᴇʀ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs
 <b>›› /my_super_plan :</b> ᴄʜᴇᴄᴋ sᴜᴘᴇʀ ᴘʀᴇᴍɪᴜᴍ sᴛᴀᴛᴜs"""
-
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>")
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+#------------------------------------------------------------------------------------------------------------------------------------------------
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None")
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+#------------------------------------------------------------------------------------------------------------------------------------------------
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
-
+#------------------------------------------------------------------------------------------------------------------------------------------------
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
