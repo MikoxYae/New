429315bb13ed6c99e5c8f1fd2053be52805efa78
Yae Miko <aloksinghkumar756@gmail.com>
2026-04-15 09:26:49 +0530

    feat: add settings.py plugin with /settings command

---
+++ plugins/settings.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bot import Bot
+from helper_func import admin
+
+SETTINGS_PIC = "https://graph.org/file/d18515f99d522b3ee4e6f-876aedcb4f5dde2d4e.jpg"
+
+
+@Bot.on_message(filters.command('settings') & filters.private & admin)
+async def settings_command(client: Client, message: Message):
+    await message.reply_photo(
+        photo=SETTINGS_PIC,
+        caption="<b>⚙️ Settings Panel</b>\n\nSelect a category to manage:",
+        reply_markup=InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("👑 Admin", callback_data="stg_admin"),
+                InlineKeyboardButton("🚫 Ban Users", callback_data="stg_ban")
+            ]
+        ])
+    )
